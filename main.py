55c5400edc07f8afee90d257fd951ccace376c98
robkenhow77-sigma-labs-trainee <trainee.robert.howarth@sigmalabs.co.uk>
2025-03-13 11:09:21 +0000

    api

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from os import urandom, environ as ENV
 from time import sleep
+from datetime import datetime
 
 from dotenv import load_dotenv
 from requests import get, post
@@ -9,6 +10,23 @@ from flask import Flask, redirect, request, session, render_template
 load_dotenv
 app = Flask(__name__)
 app.secret_key = urandom(20)
+
+
+def make_activities_table(data: dict):
+    """Makes the table for the main page"""
+    html = ""
+    for row in data:
+        date_time = datetime.fromisoformat(row["start_date"])
+        date_time = datetime.strftime(date_time, "%d/%m/%Y, %H:%M")
+        html += f"""
+            <tr>
+                <td>{date_time}</td>
+                <td>{row["sport_type"]}</td>
+                <td>{row["elapsed_time"]}</td>
+                <td>{row["kudos_count"]}</td>
+            </tr>
+            """
+    return html
 
 
 @app.route("/")
@@ -23,14 +41,18 @@ def callback():
     session["code"] = code
     POST = f"https://www.strava.com/oauth/token?client_id={ENV["CLIENT_ID"]}&client_secret={ENV["CLIENT_SECRET"]}&code={code}&grant_type=authorization_code"
     res = post(POST)
-    print(res)
     if res.status_code != 200:
         return "Bad request", 400
     session["access_token"] = res.json()["access_token"]
-    return redirect("/welcome")
+    return redirect("/main")
 
 
-@app.route("/welcome")
+@app.route("/main")
 def welcome():
-    res = get("https://www.strava.com/api/v3/activities", headers={"Authorization": f'Bearer {session["access_token"]}'})
-    return res.json()
+    """API call to get strava information"""
+    res = get("https://www.strava.com/api/v3/activities?after=1735689600", headers={"Authorization": f'Bearer {session["access_token"]}'})
+    data = res.json()
+    table_html = make_activities_table(data)
+    """Adding a variable to a template"""
+    print(ENV["API_KEY"])
+    return render_template('main_page.html', table_data=table_html, API_KEY=ENV["API_KEY"])
